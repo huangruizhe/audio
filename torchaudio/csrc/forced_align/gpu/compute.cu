84870310626b956459fdfa5dcc4a28043908966d
huangruizhe <eraser567@163.com>
2023-07-30 16:05:42 -0400

    Update compute.cu
    added weighted ctc decoding to cuda kernel

--- torchaudio/csrc/forced_align/gpu/compute.cu
+++ torchaudio/csrc/forced_align/gpu/compute.cu
@@ -52,7 +52,10 @@ __global__ void falign_cuda_step_kernel(
   if (t == 0) {
     for (unsigned int i = start + threadIdx.x; i < end; i += blockDim.x) {
       int labelIdx = (i % 2 == 0) ? blank : targets_a[batchIndex][i / 2];
-      alphas_a[curIdxOffset][i] = logProbs_a[batchIndex][0][labelIdx];
+      if labelIdx == blank:
+          alphas_a[curIdxOffset][i] = logProbs_a[batchIndex][0][labelIdx] + inter_word_blank_penalty;
+      else:
+          alphas_a[curIdxOffset][i] = logProbs_a[batchIndex][0][labelIdx];
     }
     return;
   }
@@ -64,7 +67,7 @@ __global__ void falign_cuda_step_kernel(
   threadMax = kNegInfinity;
   if (start == 0 && threadIdx.x == 0) {
     alphas_a[curIdxOffset][0] =
-        alphas_a[prevIdxOffset][0] + logProbs_a[batchIndex][t][blank];
+        alphas_a[prevIdxOffset][0] + logProbs_a[batchIndex][t][blank] + inter_word_blank_penalty;
     threadMax = max(threadMax, alphas_a[curIdxOffset][0]);
     backPtrBuffer_a[backPtrBufferLen][0] = 0;
   }
@@ -76,6 +79,15 @@ __global__ void falign_cuda_step_kernel(
     scalar_t x1 = alphas_a[prevIdxOffset][i - 1];
     scalar_t x2 = kNegInfinity;
     int labelIdx = (i % 2 == 0) ? blank : targets_a[batchIndex][i / 2];
+
+    if labelIdx == blank:
+        if i == 0 or i == S - 1 or i / 2 + 1 in word_start_idx:  # inter-word blank
+            blank_penalty = inter_word_blank_penalty
+        else:  # intra-word blank
+            blank_penalty = intra_word_blank_penalty
+    else:
+        blank_penalty = 0
+
     if (i % 2 != 0 && i != 1 &&
         targets_a[batchIndex][i / 2] != targets_a[batchIndex][i / 2 - 1]) {
       x2 = alphas_a[prevIdxOffset][i - 2];
@@ -91,7 +103,7 @@ __global__ void falign_cuda_step_kernel(
       result = x0;
       backPtrBuffer_a[backPtrBufferLen][i] = 0;
     }
-    alphas_a[curIdxOffset][i] = result + logProbs_a[batchIndex][t][labelIdx];
+    alphas_a[curIdxOffset][i] = result + logProbs_a[batchIndex][t][labelIdx] + blank_penalty;
     threadMax = max(threadMax, alphas_a[curIdxOffset][i]);
   }
   scalar_t maxResult = BlockReduce(tempStorage).Reduce(threadMax, cub::Max());
