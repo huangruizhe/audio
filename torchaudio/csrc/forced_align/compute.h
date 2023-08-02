0c51a109439ba1345be33f98f75e4169565493c0
huangruizhe <eraser567@163.com>
2023-08-02 14:10:28 -0400

    Update compute.h

--- torchaudio/csrc/forced_align/compute.h
+++ torchaudio/csrc/forced_align/compute.h
@@ -7,4 +7,7 @@ std::tuple<torch::Tensor, torch::Tensor> forced_align(
     const torch::Tensor& targets,
     const torch::Tensor& inputLengths,
     const torch::Tensor& targetLengths,
-    const int64_t blank);
+    const int64_t blank,
+    double inter_word_blank_penalty,
+    double inter_word_blank_penalty,
+    const torch::Tensor& word_start_index);
